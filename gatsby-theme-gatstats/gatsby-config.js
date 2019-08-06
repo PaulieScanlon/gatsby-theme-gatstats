456a95eb21daa4db3e794eaeb5dc922a82847572
pauliescanlon <pauliescanlon@gmail.com>
2019-08-06 15:15:21 +0100

    testing with demo pages

--- gatsby-theme-gatstats/gatsby-config.js
+++ gatsby-theme-gatstats/gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "pages",
-        path: `${__dirname}/src/pages`,
+        path: path.join(__dirname, `src/pages`),
       },
     },
     {
