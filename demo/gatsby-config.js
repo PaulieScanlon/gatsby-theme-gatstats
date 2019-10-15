f8920af3ccfeb32d704d9e8e7dbd7362590b4036
pauliescanlon <pauliescanlon@gmail.com>
2019-10-15 08:53:15 +0100

    alpha-0.0.1

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -7,7 +7,7 @@
 module.exports = {
   siteMetadata: {
     title: "Gat Stats",
-    description: "Data viz for blogs",
+    description: "Data viz for blogs - alpha-0.0.1",
     config: {
       sideBarWidth: 250,
     },
