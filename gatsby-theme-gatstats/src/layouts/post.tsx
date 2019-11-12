bef61b18fa0d7428343f3bd4439bc9145eface86
pauliescanlon <pauliescanlon@gmail.com>
2019-11-12 08:51:32 +0000

    change layout post

--- gatsby-theme-gatstats/src/layouts/post.tsx
+++ gatsby-theme-gatstats/src/layouts/post.tsx
@@ -1,11 +1,61 @@
 /** @jsx jsx */
-
-import { jsx } from 'theme-ui'
+import * as React from 'react'
+import { jsx, Styled } from 'theme-ui'
 import { graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
+import Img from 'gatsby-image'
+
+import { formatDate } from '..//utils'
 
 const Post = ({ data: { mdx } }: any) => {
-  return <MDXRenderer>{mdx.body}</MDXRenderer>
+  const { timeToRead, wordCount } = mdx
+  const { title, date, featuredImage } = mdx.frontmatter
+
+  return (
+    <React.Fragment>
+      {featuredImage && (
+        <Styled.div
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            width: '100%',
+            height: [250, 300],
+            overflow: 'hidden',
+            mb: 4
+          }}
+        >
+          <Img
+            fluid={featuredImage.childImageSharp.fluid}
+            alt={title}
+            style={{
+              display: 'block',
+              width: '100%',
+              height: '100%'
+            }}
+          />
+        </Styled.div>
+      )}
+      <Styled.div
+        sx={{
+          fontSize: 2,
+          fontFamily: 'body',
+          color: 'secondary',
+          mb: 3
+        }}
+      >
+        {formatDate(date)}
+      </Styled.div>
+      <Styled.h1>{title}</Styled.h1>
+      <Styled.p
+        sx={{
+          mb: 4,
+          color: 'textMuted'
+        }}
+      >{`${timeToRead} min read / ${wordCount.words} words`}</Styled.p>
+
+      <MDXRenderer>{mdx.body}</MDXRenderer>
+    </React.Fragment>
+  )
 }
 
 export const contentQuery = graphql`
@@ -22,6 +72,16 @@ export const contentQuery = graphql`
         title
         tags
         date(formatString: "MMMM DD, YYYY")
+        featuredImage {
+          childImageSharp {
+            fluid {
+              aspectRatio
+              src
+              srcSet
+              sizes
+            }
+          }
+        }
       }
     }
   }
