0d336e448b080bddc38ef22fabd450650c21938d
pauliescanlon <pauliescanlon@gmail.com>
2019-11-29 08:19:16 +0000

    hard code meta tags 2

--- @pauliescanlon/gatsby-theme-gatstats/src/components/Seo/Seo.tsx
+++ @pauliescanlon/gatsby-theme-gatstats/src/components/Seo/Seo.tsx
@@ -26,77 +26,30 @@ export const Seo: React.FC<ISeoProps> = ({
   }`
 
   return (
-    <Helmet
-      htmlAttributes={{
-        lang
-      }}
-      title={title}
-      titleTemplate={formatTitleTemplate}
-    >
+    <Helmet title={title} titleTemplate={formatTitleTemplate}>
+      <html lang={lang} />
+      <meta name="description" content={description} />
+      <meta name="image" content={`${siteURL}/${image ? image : ''}`} />
       <meta
-        data-react-helmet="true"
-        property="og:title"
-        content={formatTitleTemplate}
-      />
-      <meta data-react-helmet="true" property="og:url" content={siteURL} />
-      <meta
-        data-react-helmet="true"
-        property="og:description"
-        content={description}
-      />
-      <meta
-        data-react-helmet="true"
-        property="og:image"
-        content={`${siteURL}/${image ? image : ''}`}
-      />
-      <meta data-react-helmet="true" property="og:type" content="website" />
-      <meta
-        data-react-helmet="true"
-        property="og:image:alt"
-        content={description}
-      />
-      <meta
-        data-react-helmet="true"
-        name="twitter:card"
-        content={`${siteURL}/${image ? image : ''}`}
-      />
-      <meta
-        data-react-helmet="true"
-        name="twitter:title"
-        content={formatTitleTemplate}
-      />
-      <meta data-react-helmet="true" name="twitter:url" content={siteURL} />
-      <meta
-        data-react-helmet="true"
-        name="twitter:description"
-        content={description}
-      />
-      <meta
-        data-react-helmet="true"
-        name="twitter:image"
-        content={`${siteURL}/${image ? image : ''}`}
-      />
-      <meta
-        data-react-helmet="true"
-        name="twitter:image:alt"
-        content={description}
-      ></meta>
-
-      <meta
-        data-react-helmet="true"
-        name="twitter:creator"
-        content={siteURL}
-      ></meta>
-      <meta
-        data-react-helmet="true"
         name="gatsby-theme"
         content="@pauliescanlon/gatsby-theme-gatstats"
       />
-      <meta
-        data-react-helmet="true"
-        name="keywords"
-        content={keywords.join(', ')}
-      />
+      <meta name="keywords" content={keywords.join(', ')} />
+      {/* Facebook */}
+      <meta property="og:title" content={formatTitleTemplate} />
+      <meta property="og:url" content={siteURL} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={`${siteURL}/${image ? image : ''}`} />
+      <meta property="og:type" content="website" />
+      <meta property="og:image:alt" content={description} />
+      {/* Twitter */}
+      <meta name="twitter:card" content={`${siteURL}/${image ? image : ''}`} />
+      <meta name="twitter:title" content={formatTitleTemplate} />
+      <meta name="twitter:url" content={siteURL} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={`${siteURL}/${image ? image : ''}`} />
+      <meta name="twitter:image:alt" content={description}></meta>
+      <meta name="twitter:creator" content={siteURL}></meta>
     </Helmet>
   )
 }
