595bfe07b2d5810d8a9f30de55775e46f600e738
pauliescanlon <pauliescanlon@gmail.com>
2019-12-01 13:02:14 +0000

    add favicon

--- @pauliescanlon/gatsby-theme-gatstats/src/components/Seo/Seo.tsx
+++ @pauliescanlon/gatsby-theme-gatstats/src/components/Seo/Seo.tsx
@@ -24,10 +24,27 @@ export const Seo: React.FC<ISeoProps> = ({
   }`
 
   return (
-    <Helmet title={title} titleTemplate={formatTitleTemplate}>
+    <Helmet
+      title={title}
+      titleTemplate={formatTitleTemplate}
+      link={[
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '16x16',
+          href: `${`${siteURL}/images/favicon-16x16.png`}`
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
+          href: `${`${siteURL}/images/favicon-32x32.png`}`
+        }
+      ]}
+    >
       <html lang={lang} />
       <meta name="description" content={description} />
-      <meta name="image" content={`${siteURL}/${image ? image : ''}`} />
+      <meta name="image" content={`${siteURL}/images/${image ? image : ''}`} />
       <meta name="image:alt" content={description} />
       <meta
         name="gatsby-theme"
@@ -38,7 +55,10 @@ export const Seo: React.FC<ISeoProps> = ({
       <meta property="og:title" content={formatTitleTemplate} />
       <meta property="og:url" content={siteURL} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={`${siteURL}/${image ? image : ''}`} />
+      <meta
+        property="og:image"
+        content={`${siteURL}/images/${image ? image : ''}`}
+      />
       <meta property="og:type" content="website" />
       <meta property="og:image:alt" content={description} />
       {/* Twitter */}
@@ -46,7 +66,10 @@ export const Seo: React.FC<ISeoProps> = ({
       <meta name="twitter:title" content={formatTitleTemplate} />
       <meta name="twitter:url" content={siteURL} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={`${siteURL}/${image ? image : ''}`} />
+      <meta
+        name="twitter:image"
+        content={`${siteURL}/images/${image ? image : ''}`}
+      />
       <meta name="twitter:image:alt" content={description}></meta>
       <meta name="twitter:creator" content={siteURL}></meta>
     </Helmet>
