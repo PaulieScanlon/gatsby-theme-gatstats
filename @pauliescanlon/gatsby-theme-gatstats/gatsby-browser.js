cfa2d8d77216ba2369072364ece42482a73877b4
pauliescanlon <pauliescanlon@gmail.com>
2019-12-02 14:29:22 +0000

    add gatsby-browser to resolve page jank

---
+++ @pauliescanlon/gatsby-theme-gatstats/gatsby-browser.js
@@ -0,0 +1,17 @@
+const transitionDelay = 250
+
+exports.shouldUpdateScroll = ({
+  routerProps: { location },
+  getSavedScrollPosition
+}) => {
+  if (location.action === 'PUSH') {
+    window.setTimeout(() => window.scrollTo(0, 0), transitionDelay)
+  } else {
+    const savedPosition = getSavedScrollPosition(location)
+    window.setTimeout(
+      () => window.scrollTo(...(savedPosition || [0, 0])),
+      transitionDelay
+    )
+  }
+  return false
+}
