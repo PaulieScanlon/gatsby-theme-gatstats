0db97e4e28038406d2447102cb02af4a61365158
pauliescanlon <pauliescanlon@gmail.com>
2019-12-27 11:27:29 +0000

    fix seo image page

--- @pauliescanlon/gatsby-theme-gatstats/src/components/Seo/Seo.tsx
+++ @pauliescanlon/gatsby-theme-gatstats/src/components/Seo/Seo.tsx
@@ -32,19 +32,19 @@ export const Seo: React.FC<ISeoProps> = ({
           rel: 'icon',
           type: 'image/png',
           sizes: '16x16',
-          href: `${`${siteURL}/images/favicon-16x16.png`}`
+          href: `${siteURL}/images/favicon-16x16.png`
         },
         {
           rel: 'icon',
           type: 'image/png',
           sizes: '32x32',
-          href: `${`${siteURL}/images/favicon-32x32.png`}`
+          href: `${siteURL}/images/favicon-32x32.png`
         }
       ]}
     >
       <html lang={lang} />
       <meta name="description" content={description} />
-      <meta name="image" content={`${siteURL}/images/${image ? image : ''}`} />
+      <meta name="image" content={`${siteURL}/${image ? image : ''}`} />
       <meta name="image:alt" content={description} />
       <meta
         name="gatsby-theme"
@@ -55,10 +55,7 @@ export const Seo: React.FC<ISeoProps> = ({
       <meta property="og:title" content={formatTitleTemplate} />
       <meta property="og:url" content={siteURL} />
       <meta property="og:description" content={description} />
-      <meta
-        property="og:image"
-        content={`${siteURL}/images/${image ? image : ''}`}
-      />
+      <meta property="og:image" content={`${siteURL}/${image ? image : ''}`} />
       <meta property="og:type" content="website" />
       <meta property="og:image:alt" content={description} />
       {/* Twitter */}
@@ -66,10 +63,7 @@ export const Seo: React.FC<ISeoProps> = ({
       <meta name="twitter:title" content={formatTitleTemplate} />
       <meta name="twitter:url" content={siteURL} />
       <meta name="twitter:description" content={description} />
-      <meta
-        name="twitter:image"
-        content={`${siteURL}/images/${image ? image : ''}`}
-      />
+      <meta name="twitter:image" content={`${siteURL}/${image ? image : ''}`} />
       <meta name="twitter:image:alt" content={description}></meta>
       <meta name="twitter:creator" content={siteURL}></meta>
     </Helmet>
--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -2,26 +2,26 @@
 
 module.exports = {
   siteMetadata: {
-    title: 'Introducing GatStats',
+    title: "Introducing GatStats",
     description:
-      'GatStats is a dashboard for your tech blog, or a dashblog if you prefer? ',
-    keywords: ['GatsbyJs', 'React', 'theme-ui'],
-    siteURL: 'https://gatsby-theme-gatstats.netlify.com',
-    siteImage: 'gatstats-open-graph-image.jpg',
+      "GatStats is a dashboard for your tech blog, or a dashblog if you prefer? ",
+    keywords: ["GatsbyJs", "React", "theme-ui"],
+    siteURL: "https://gatsby-theme-gatstats.netlify.com",
+    siteImage: "images/gatstats-open-graph-image.jpg",
     config: {
       headerHeight: 64,
       sideBarWidth: 240,
-      twitter: 'pauliescanlon',
-      github: 'pauliescanlon'
+      twitter: "pauliescanlon",
+      github: "pauliescanlon"
     }
   },
   plugins: [
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: "gatsby-plugin-google-analytics",
       options: {
-        trackingId: 'UA-76055934-3'
+        trackingId: "UA-76055934-3"
       }
     },
-    '@pauliescanlon/gatsby-theme-gatstats'
+    "@pauliescanlon/gatsby-theme-gatstats"
   ]
-}
+};
