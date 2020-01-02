03b535830bb56141e52d686adacb8cfe9da539ef
pauliescanlon <pauliescanlon@gmail.com>
2020-01-02 08:44:37 +0000

    fix hardcode tweet url

--- @pauliescanlon/gatsby-theme-gatstats/src/components/Twitter/Tweet.tsx
+++ @pauliescanlon/gatsby-theme-gatstats/src/components/Twitter/Tweet.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 import { jsx, Styled } from 'theme-ui'
 
 export interface ITweetProps {
-  /** The YouTube id */
+  /** The Tweet id */
   id: string
 }
 
@@ -11,7 +11,7 @@ export const Tweet: React.FC<ITweetProps> = ({ id }) => {
   return (
     <Styled.div>
       <blockquote className="twitter-tweet">
-        <a href={`https://twitter.com/gatsbyjs/status/${id}`} />
+        <a href={`https://twitter.com/${id}`} />
       </blockquote>
     </Styled.div>
   )
